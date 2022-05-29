a36808c7c21c4cca0bd001f95da6b4c1dea4d2d8
DESKTOP-FNJ8BDH\Usuario <alvaro.boza@gmail.com>
2022-05-29 11:36:21 +0200

    SideNav Avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Aprendiendo Angular';
+
+  menu = {
+    "navitems": [
+      {
+        "linkText": "About",
+        "parentLink": "/about",
+        "menu": false,
+        "submenu": []
+      },
+      {
+        "linkText": "Codepens",
+        "parentLink": "",
+        "menu": false,
+        "submenu": [
+          {
+            "childtext": "Link 1",
+            "link": "www.google.es"
+          },
+          {
+            "childtext": "Link 2",
+            "link": "www.google.es"
+          },
+          
+        ]
+      },
+      {
+        "linkText": "Navigation",
+        "parentLink": "",
+        "menu": false,
+        "submenu": [
+          {
+            "childtext": "Pure CSS Menu",
+            "link": "www.google.es"
+          },
+          {
+            "childtext": "User Selection",
+            "link": "www.google.es"
+          }
+        ]
+      },
+    ]
+  }
+
+  result: any = [];
+
+  constructor() {
+    for (let key in this.menu.navitems) {
+      if (this.menu.navitems.hasOwnProperty(key)) {
+        this.result.push(this.menu.navitems[key]);
+      }
+    }
+  }
+
+  ngOnInit() {}
+
+  ddToggle(i: string | number) {
+    this.result[i].menu = !this.result[i].menu;
+    console.log(this.result[i].menu);
+  }
+
 }
