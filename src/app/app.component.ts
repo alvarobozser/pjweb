18cb81cb46d088afc01335b524497ec03d788be6
DESKTOP-FNJ8BDH\Usuario <alvaro.boza@gmail.com>
2022-06-11 14:10:49 +0200

    Cabecera y Menu Despegable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,64 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  menu = {
-    "navitems": [
-      {
-        "linkText": "About",
-        "parentLink": "/about",
-        "menu": false,
-        "submenu": []
-      },
-      {
-        "linkText": "Codepens",
-        "parentLink": "",
-        "menu": false,
-        "submenu": [
-          {
-            "childtext": "Link 1",
-            "link": "www.google.es"
-          },
-          {
-            "childtext": "Link 2",
-            "link": "www.google.es"
-          },
-          
-        ]
-      },
-      {
-        "linkText": "Navigation",
-        "parentLink": "",
-        "menu": false,
-        "submenu": [
-          {
-            "childtext": "Pure CSS Menu",
-            "link": "www.google.es"
-          },
-          {
-            "childtext": "User Selection",
-            "link": "www.google.es"
-          }
-        ]
-      },
-    ]
-  }
-
-  result: any = [];
-
+  
   constructor() {
-    for (let key in this.menu.navitems) {
-      if (this.menu.navitems.hasOwnProperty(key)) {
-        this.result.push(this.menu.navitems[key]);
-      }
-    }
+
   }
 
   ngOnInit() {}
 
-  ddToggle(i: string | number) {
-    this.result[i].menu = !this.result[i].menu;
-    console.log(this.result[i].menu);
-  }
+
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialExampleModule } from 'src/material.module';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialExampleModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
